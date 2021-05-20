879a929e71482dcca179cfe4d30e362c14b5bca8
Mary Toth <maryarylynn@gmail.com>
2021-05-20 17:28:11 -0400

    draw feature sort of works

--- public/main.js
+++ public/main.js
@@ -1,7 +1,43 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  //Variables
+
+  let player1Choice = ''
+  let player2Choice = ''
+
+  // let player1Rock = document.querySelector('section.player1 button.rock')
+  // let player1Paper = document.querySelector('section.player1 button.paper')
+  // let player1Scissors = document.querySelector(
+  //   'section.player1 button.scissors'
+  // )
+
+  // let player2Rock = document.querySelector('section.player2 button.rock')
+  // let player2Paper = document.querySelector('section.player2 button.paper')
+  // let player2Scissors = document.querySelector(
+  //   'section.player2 button.scissors'
+  // )
+
+  function handleDrawClick1(event) {
+    player1Choice = 'Rock'
+    // player2Choice = 'rock'
+
+    if (player1Choice === player2Choice) {
+      window.alert(`Draw! Both players chose ${player1Choice}!`)
+    }
   }
+  function handleDrawClick2(event) {
+    player2Choice = 'Rock'
+    if (player2Choice === player1Choice) {
+      window.alert(`Draw! Both players chose ${player1Choice}!`)
+    }
+  }
+
+  document
+    .querySelector('section.player1 button.rock')
+    .addEventListener('click', handleDrawClick1)
+
+  document
+    .querySelector('section.player2 button.rock')
+    .addEventListener('click', handleDrawClick2)
 }
 
 document.addEventListener('DOMContentLoaded', main)
